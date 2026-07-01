0eff891a8cfc2a9a4ea27863bd9ce6c61cc1e6ce
Nova X Hub <thunganyeudau1111@gmail.com>
2026-07-01 20:34:42 +0700

    Update NotifyApi.lua

--- NotifyApi.lua
+++ NotifyApi.lua
@@ -1,4 +1,4 @@
 -- Web NQP
-loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/4iua41k6"))()
+loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/g5idevc2"))()
 -- web trieu
 loadstring(game:HttpGet("https://xhider.xyz/raw/anhdepzai/NotiApiTrieu.txt"))()
