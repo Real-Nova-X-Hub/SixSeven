2badc49f8993671af7d66fa5839f661d09d740be
Nova X Hub <thunganyeudau1111@gmail.com>
2026-07-01 10:56:36 +0700

    Create NotifyApi.lua

---
+++ NotifyApi.lua
@@ -0,0 +1,4 @@
+-- Web NQP
+loadstring(game:HttpGet("https://xhider.xyz/raw/anhdepzai/NotiApiNQP.txt"))()
+-- web trieu
+loadstring(game:HttpGet("https://xhider.xyz/raw/anhdepzai/NotiApiTrieu.txt"))()
